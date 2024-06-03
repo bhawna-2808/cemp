842f6c2cac7998f0dfba15bf267e8e0000732641
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-03 17:43:40 +0530

    d

--- login/urls.py
+++ login/urls.py
@@ -7,4 +7,6 @@ from .views import *
 urlpatterns = [
     path('add_document/', AddDocumentAPIView.as_view(), name='file-upload'),
     path('view_files/', file_list_view, name='file-list'),
+    path('edit/<str:file_name>/', EditDocumentView.as_view(), name='edit-file'),
+
 ]
