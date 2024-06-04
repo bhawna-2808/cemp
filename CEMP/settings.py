037f253aed27e1a82ac5056e87e68e92220aff41
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-04 18:38:33 +0530

    d

--- CEMP/settings.py
+++ CEMP/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import pytesseract
+
+# Specify the Tesseract executable path
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
