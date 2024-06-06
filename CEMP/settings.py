d02d0b15a698e6900bbab65cf1ac67bd62f4c3cb
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-06 11:17:13 +0530

    d

--- CEMP/settings.py
+++ CEMP/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zzlyv3c_ey8z2+gb433rxhk-gis7q43wpew$316+)bt3++ab@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -69,30 +70,8 @@ MIDDLEWARE = [
     # ...
 ]
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-    '193.46.199.128'
-]
 
-# # Debug Toolbar settings
-# DEBUG_TOOLBAR_PANELS = [
-#     'debug_toolbar.panels.versions.VersionsPanel',
-#     'debug_toolbar.panels.timer.TimerPanel',
-#     'debug_toolbar.panels.settings.SettingsPanel',
-#     'debug_toolbar.panels.headers.HeadersPanel',
-#     'debug_toolbar.panels.request.RequestPanel',
-#     'debug_toolbar.panels.sql.SQLPanel',
-#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-#     'debug_toolbar.panels.templates.TemplatesPanel',
-#     'debug_toolbar.panels.cache.CachePanel',
-#     'debug_toolbar.panels.signals.SignalsPanel',
-#     'debug_toolbar.panels.logging.LoggingPanel',
-#     'debug_toolbar.panels.redirects.RedirectsPanel',
-# ]
 
-# DEBUG_TOOLBAR_CONFIG = {
-#     'INTERCEPT_REDIRECTS': False,
-# }
 
 ROOT_URLCONF = 'CEMP.urls'
 
