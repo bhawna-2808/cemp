b0d21178dd28825eb536327e1b13ef18016568fd
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-06 12:52:15 +0530

    d

--- CEMP/settings.py
+++ CEMP/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'CEMP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
