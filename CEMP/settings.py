188c3fba7c1a4bf6f8376f2a162d15051678c5ac
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-06 09:52:22 +0530

    d

--- CEMP/settings.py
+++ CEMP/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -59,7 +60,37 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'login.middleware.ExceptionLoggingMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    # ...
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    '193.46.199.128'
+]
+
+# Debug Toolbar settings
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
 
 ROOT_URLCONF = 'CEMP.urls'
 
@@ -102,7 +133,7 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': True,
         },
-        'myapp': {  # Replace 'myapp' with your app name
+        'login': {  
             'handlers': ['file', 'console'],
             'level': 'DEBUG',
             'propagate': True,
---
+++ login/middleware.py
@@ -0,0 +1,17 @@
+# myapp/middleware.py
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ExceptionLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        logger.error('Unhandled exception occurred', exc_info=exception)
+        return None
