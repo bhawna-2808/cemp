6e943c8c6f4828c4ce66905846061888b72e3d73
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-06 15:05:43 +0530

    d

--- CEMP/settings.py
+++ CEMP/settings.py
@@ -62,8 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'login.middleware.ExceptionLoggingMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+ 
 
     # ...
 ]
