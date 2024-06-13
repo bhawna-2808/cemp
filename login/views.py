9a5ac7c975a1457abc7e5ea00dce4278aaf47965
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-06-13 16:32:11 +0530

    imae change

--- login/views.py
+++ login/views.py
@@ -157,29 +157,29 @@ class AddDocumentAPIView(APIView):
             logger.error(f"Error extracting text from DOCX: {str(e)}")
             return f'Error extracting text from DOCX: {str(e)}', []
 
-    def extract_text_from_image(self, file_path, config='--psm 6',grayscale=True):
+    def extract_text_from_image(self, file_path):
         try:
             logger.info(f"Extracting text from image file: {file_path}")
 
             img = Image.open(file_path)
              # Preprocessing (adjust based on image characteristics)
-            if grayscale:
-                img = img.convert('L')  # Convert to grayscale
+            # if grayscale:
+            #     img = img.convert('L')  # Convert to grayscale
 
-            # Logging for debugging
-            logger.info(f"Image shape after grayscale conversion: {img.size}")
-            logger.info(f"Image data type after grayscale conversion: {img.mode}")
-                # Noise reduction (example)
-            # Apply a median filter to reduce noise
-            img = cv2.medianBlur(np.array(img), 3)
-            # Binarization (example)
-            # Convert to binary image for better text extraction
-            thresh = cv2.threshold(np.array(img), 127, 255, cv2.THRESH_BINARY)[1]
+            # # Logging for debugging
+            # logger.info(f"Image shape after grayscale conversion: {img.size}")
+            # logger.info(f"Image data type after grayscale conversion: {img.mode}")
+            #     # Noise reduction (example)
+            # # Apply a median filter to reduce noise
+            # img = cv2.medianBlur(np.array(img), 3)
+            # # Binarization (example)
+            # # Convert to binary image for better text extraction
+            # thresh = cv2.threshold(np.array(img), 127, 255, cv2.THRESH_BINARY)[1]
 
-            #  Optional: Additional preprocessing steps like skew correction or layout analysis
+            # #  Optional: Additional preprocessing steps like skew correction or layout analysis
 
-            # Text extraction with Tesseract
-            text = pytesseract.image_to_string(thresh, config=config)
+            # # Text extraction with Tesseract
+            text = pytesseract.image_to_string(img)
             # text = pytesseract.image_to_string(img)
 
             logger.info("Text extraction from image successful.")
